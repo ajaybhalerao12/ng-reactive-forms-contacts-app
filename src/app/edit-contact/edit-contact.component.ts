94d386783ddf7069ac4961c4677e5806ddc3a8f9
ajaybhalerao12 <ajaybhalerao12@gmail.com>
2024-11-04 14:07:34 +0530

    feat: Refactor edit contact component to use FormBuilder for reactive form initialization

--- src/app/edit-contact/edit-contact.component.ts
+++ src/app/edit-contact/edit-contact.component.ts
@@ -2,7 +2,7 @@ import { Address } from './../contacts/contact.model';
 import { CommonModule } from '@angular/common';
 import { ContactsService } from './../contacts/contacts.service';
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 
 @Component({
@@ -13,29 +13,51 @@ import { ActivatedRoute, Router, RouterLink } from '@angular/router';
   styleUrl: './edit-contact.component.css',
 })
 export class EditContactComponent implements OnInit {
-  contactForm = new FormGroup({
-    id: new FormControl(),
-    firstName: new FormControl(),
-    lastName: new FormControl(),
-    dateOfBirth: new FormControl(),
-    favoritesRanking: new FormControl(),
-    phone: new FormGroup({
-      phoneNumber: new FormControl(),
-      phoneType: new FormControl(),
-    }),
-    address: new FormGroup({
-      streetAddress: new FormControl(),
-      city: new FormControl(),
-      state: new FormControl(),
-      postalCode: new FormControl(),
-      addressType: new FormControl(),
-    }),
-  });
+  contactForm: FormGroup ;
+
+  // contactForm = new FormGroup({
+  //   id: new FormControl(),
+  //   firstName: new FormControl(),
+  //   lastName: new FormControl(),
+  //   dateOfBirth: new FormControl(),
+  //   favoritesRanking: new FormControl(),
+  //   phone: new FormGroup({
+  //     phoneNumber: new FormControl(),
+  //     phoneType: new FormControl(),
+  //   }),
+  //   address: new FormGroup({
+  //     streetAddress: new FormControl(),
+  //     city: new FormControl(),
+  //     state: new FormControl(),
+  //     postalCode: new FormControl(),
+  //     addressType: new FormControl(),
+  //   }),
+  // });
   constructor(
     private route: ActivatedRoute,
+    private fb: FormBuilder,
     private contactService: ContactsService,
     private router: Router
-  ) {}
+  ) {
+    this.contactForm = this.fb.group({
+      id: [''],
+      firstName: [''],
+      lastName: [''],
+      dateOfBirth: [''],
+      favoritesRanking: [''],
+      phone: this.fb.group({
+        phoneNumber: [''],
+        phoneType: [''],
+      }),
+      address: this.fb.group({
+        streetAddress: [''],
+        city: [''],
+        state: [''],
+        postalCode: [''],
+        addressType: [''],
+      }),
+    });
+  }
 
   ngOnInit() {
     const contactId = this.route.snapshot.params['id'];
@@ -43,21 +65,22 @@ export class EditContactComponent implements OnInit {
 
     this.contactService.getContact(contactId).subscribe((contact) => {
       if (contact) {
-        this.contactForm.controls.id.setValue(contact.id);
-        this.contactForm.controls.firstName.setValue(contact.firstName);
-        this.contactForm.controls.lastName.setValue(contact.lastName);
-        this.contactForm.controls.dateOfBirth.setValue(contact.dateOfBirth);
-        this.contactForm.controls.favoritesRanking.setValue(
-          contact.favoritesRanking
-        );
-        this.contactForm.controls.phone.controls.phoneNumber.setValue(contact.phone.phoneNumber);
-        this.contactForm.controls.phone.controls.phoneType.setValue(contact.phone.phoneType);
+        this.contactForm.setValue(contact);
+        // this.contactForm.controls.id.setValue(contact.id);
+        // this.contactForm.controls.firstName.setValue(contact.firstName);
+        // this.contactForm.controls.lastName.setValue(contact.lastName);
+        // this.contactForm.controls.dateOfBirth.setValue(contact.dateOfBirth);
+        // this.contactForm.controls.favoritesRanking.setValue(
+        //   contact.favoritesRanking
+        // );
+        // this.contactForm.controls.phone.controls.phoneNumber.setValue(contact.phone.phoneNumber);
+        // this.contactForm.controls.phone.controls.phoneType.setValue(contact.phone.phoneType);
 
-        this.contactForm.controls.address.controls.streetAddress.setValue(contact.address.streetAddress);
-        this.contactForm.controls.address.controls.city.setValue(contact.address.city);
-        this.contactForm.controls.address.controls.state.setValue(contact.address.state);
-        this.contactForm.controls.address.controls.postalCode.setValue(contact.address.postalCode);
-        this.contactForm.controls.address.controls.addressType.setValue(contact.address.addressType);
+        // this.contactForm.controls.address.controls.streetAddress.setValue(contact.address.streetAddress);
+        // this.contactForm.controls.address.controls.city.setValue(contact.address.city);
+        // this.contactForm.controls.address.controls.state.setValue(contact.address.state);
+        // this.contactForm.controls.address.controls.postalCode.setValue(contact.address.postalCode);
+        // this.contactForm.controls.address.controls.addressType.setValue(contact.address.addressType);
       }
     });
   }
